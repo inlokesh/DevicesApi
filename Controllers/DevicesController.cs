bbd44947e7a5f28d1489a50382765a0983cb3f71
Lokesh Subramani <lokeshsubramani@LokeshSmanisMBP.attlocal.net>
2017-05-28 11:46:36 -0400

    Initial Commit

---
+++ Controllers/DevicesController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevicesApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class DevicesController : Controller
+    {
+        // GET api/devices
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/devices/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/devices
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/devices/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/devices/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
